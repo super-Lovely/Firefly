39107961dc6954756c872dd2f76d41f481e0f175
super-Lovely <samfreetime1@gmail.com>
2026-07-12 09:45:14 +0800

    change the config about music

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -11,7 +11,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	showInNavbar: true,
 
 	// 使用方式："meting" 使用 Meting API，"local" 使用本地音乐列表
-	mode: "meting",
+	mode: "local",
 
 	// 默认音量 (0-1)
 	volume: 0.7,
@@ -50,11 +50,11 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
-				cover: "/assets/music/cover/109951169585655912.webp",
-				lrc: "",
+				name: "And I'm home",
+				artist: "喜多村英梨、野中蓝 / wowaka / wowoka",
+				url: "/assets/music/and Im home.mp3",
+				cover: "/assets/music/cover/and im home.webp",
+				lrc: "/assets/music/lrc/and Im home.lrc",
 			},
 		],
 	},
